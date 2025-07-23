64378417d3ff7c8ae21e178f2cc254c0ec7c10de
Connor Denihan <cdenihan@proton.me>
2025-07-23 10:08:21 -0400

    Update homepage design: adjust hero banner and remove unused features
    - Increased hero banner padding and adjusted layout for improved alignment.
    - Made hero image taller by setting height to 80vh for visual emphasis.
    - Removed `HomepageFeatures` component and associated code for cleaner layout.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -14,7 +13,8 @@ function HomepageHeader() {
         <header className={clsx('hero hero--primary', styles.heroBanner)} style={{
             backgroundImage: 'url("https://uroc.umd.edu/sites/uroc.umd.edu/files/DSC_0667_0.jpg")',
             backgroundSize: 'cover',
-            backgroundPosition: 'center'
+            backgroundPosition: 'center',
+            height: '80vh' // Make the hero image taller to fill more of the page
         }}>
             <div className="container">
                 <Heading as="h1" className="hero__title" style={{color: 'white'}}>
@@ -40,9 +40,6 @@ export default function Home(): ReactNode {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
